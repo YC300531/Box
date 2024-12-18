54349f471278d42db0f7f56cad345d97435b0169
YC300531 <dfyzj@163.com>
2024-12-18 21:29:23 +0800

    Delete app directory

--- app/src/main/java/com/acsbendi/requestinspectorwebview/RequestInspectorOptions.kt
+++
@@ -1,5 +0,0 @@
-package com.acsbendi.requestinspectorwebview
-
-data class RequestInspectorOptions(
-    val extraJavaScriptToInject: String = ""
-)
--- app/src/main/java/com/acsbendi/requestinspectorwebview/WebViewRequestType.kt
+++
@@ -1,5 +0,0 @@
-package com.acsbendi.requestinspectorwebview
-
-enum class WebViewRequestType {
-    FETCH, XML_HTTP, FORM, HTML
-}
